803fa57ee988aa57f8cdff1b906c3c6a9a8ae451
Dani Rod <supmercode@gmail.com>
2025-09-03 21:59:21 -0600

    Retrieve users

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,26 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable Integer id) {
+        return service.findOne(id);
+    }
+}
