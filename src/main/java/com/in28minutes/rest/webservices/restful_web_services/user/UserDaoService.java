dc9d38abc0693f378beba7dacf1cad0c21b22716
Dani Rod <supmercode@gmail.com>
2025-09-18 21:59:32 -0600

    Not found exception handling

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -10,11 +10,12 @@ import java.util.function.Predicate;
 @Component
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
+    private static int usersCount = 3;
 
     static {
-        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
     }
 
 
@@ -24,6 +25,12 @@ public class UserDaoService {
 
     public User findOne(Integer id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
-        return users.stream().filter(predicate).findFirst().get();
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
     }
 }
--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,9 +1,10 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +22,24 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable Integer id) {
+        User user = service.findOne(id);
+
+        if(user == null)
+            throw new UserNotFoundException("id: " + id);
+
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
